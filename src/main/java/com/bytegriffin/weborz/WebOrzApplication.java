b0214b65157c832bfc61e627c13949eca1ade9ff
bytegriffin <bytegriffin@126.com>
2018-07-18 17:55:07 +0800

    init

---
+++ src/main/java/com/bytegriffin/weborz/WebOrzApplication.java
@@ -0,0 +1,12 @@
+package com.bytegriffin.weborz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOrzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOrzApplication.class, args);
+	}
+}
